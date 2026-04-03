084a2425bff9e353a989552d87f68953da469db7
simransingh <simran99211@gmail.com>
2026-04-03 21:18:08 +0530

    commit 1st change

---
+++ src/main/java/com/example/tradebookingsystem/TradeBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.tradebookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeBookingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tradebookingsystem/TradeBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tradebookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

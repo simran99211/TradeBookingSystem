7fbffa6b6835f4c14edbc01aa49ddd9f039e3a61
simransingh <simran99211@gmail.com>
2026-04-07 21:33:16 +0530

    Implement basic trade booking system with CRUD functionalities

--- src/main/java/com/example/tradebookingsystem/kafka/consumer/NotificationConsumer.java
+++ src/main/java/com/example/tradebookingsystem/kafka/consumer/NotificationConsumer.java
@@ -1,28 +1,16 @@
 package com.example.tradebookingsystem.kafka.consumer;
 
-import com.example.tradebookingsystem.model.Trade;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationConsumer {
 
-    private final ObjectMapper objectMapper;
-
-    public NotificationConsumer(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
     @KafkaListener(topics = "topic1", groupId = "group1")
     public void consume(String msg) {
         try {
-            Trade trade = objectMapper.readValue(msg, Trade.class);
-            String json = objectMapper.writeValueAsString(trade);
-            System.out.println("TRADE CONSUME notification " + json);
+            System.out.println("TRADE CONSUME notification " + msg);
         } catch (Exception e) {
-            System.err.println("Failed to process message. msg=" + msg);
-            e.printStackTrace();
-        }
+            System.err.println("Failed to process message. msg=" + msg);}
     }
 }
